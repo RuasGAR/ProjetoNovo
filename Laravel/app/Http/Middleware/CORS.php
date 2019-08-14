ed869fdd6499ca97412ef7231f366f1a4f171952
vagner <vagner@ejcm.com.br>
2019-08-14 18:51:29 -0300

    # Mudanças no Back

--- Laravel/app/Http/Middleware/CORS.php
+++ Laravel/app/Http/Middleware/CORS.php
@@ -16,7 +16,10 @@ class CORS
     public function handle($request, Closure $next)
     {
         $answer = $next($request);
-        $answer->header('Access-Control-Allow-Origin','http://localhost:8100/')->header('Access-Control-Allow-Methods','GET,POST,PUT,DELETE,OPTIONS')->header('Access-Control-Allow-Headers','Authorization,Content-Type');
+        $answer->header('Access-Control-Allow-Origin','*')
+        ->header('Access-Control-Allow-Methods','GET,POST,PUT,DELETE,OPTIONS')
+        ->header('Access-Control-Allow-Headers','Authorization,Content-Type');
+        
         return $answer;
     }
 }
