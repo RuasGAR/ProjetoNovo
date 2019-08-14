5a5e33689af42e02855642e4ae82c52f6813e860
sabrinalencar <sanevesbooks@gmail.com>
2019-08-14 18:34:18 -0300

    +fab pro form de novo post

--- Ionic/src/app/app-routing.module.ts
+++ Ionic/src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
+
 const routes: Routes = [
-  {
+   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  }
+  },
+  { path: 'tabs', loadChildren: './tabs/tabs.module#TabsPageModule'},
+  { path: 'tab2', loadChildren: './tab2/tab2.module#Tab2PageModule'},
+  { path: 'tab1', loadChildren: './tab1/tab1.module#Tab1PageModule'},
+  //{ path: 'cadastro', loadChildren: './cadastro/cadastro.module#CadastroPageModule' },
+  //{ path: 'login', loadChildren: './login/login.module#LoginPageModule' },
+  //{ path: 'bemvindo', loadChildren: './bemvindo/bemvindo.module#BemvindoPageModule' }
+
 ];
 @NgModule({
   imports: [
--- Ionic/src/app/tab1/tab1.page.ts
+++ Ionic/src/app/tab1/tab1.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+import { Router } from '@angular/router'; 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
@@ -7,6 +8,11 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(public router: Router) {}
+
+  newpost(){
+    this.router.navigate(['/tab2']);
+  }
+
 
 }
