f7afde122f3bf7ac8135e2bf7306c070a9881907
RuasGAR <gabrielamaralruas@gmail.com>
2019-08-14 23:29:13 -0300

    +campos de form criados

---
+++ Ionic/src/app/newpost/newpost.page.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-newpost',
+  templateUrl: './newpost.page.html',
+  styleUrls: ['./newpost.page.scss'],
+})
+export class NewpostPage implements OnInit {
+
+  registerForm: FormGroup;  
+  constructor(public formbuilder: FormBuilder) { 
+
+    this.registerForm = this.formbuilder.group({
+      text:[null, [Validators.required, Validators.maxLength(200)]],
+      name:[null, [Validators.required, Validators.maxLength(40)]]
+    });
+    
+  
+  }
+
+  submitForm(form){
+    console.log(form);
+  }
+
+  ngOnInit() {
+  }
+}
+
+ 
+
